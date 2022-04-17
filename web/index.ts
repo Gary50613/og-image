fc69cb4a320ae55d1f3140603584fb024c4b9fe2
凱恩Kane <50613gary@gmail.com>
2022-04-17 09:38:45 +0800

    add heroku logo

--- web/index.ts
+++ web/index.ts
@@ -148,6 +148,7 @@ const imageLightOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Heroku', value: 'https://brand.heroku.com/static/media/heroku-logo-stroke.aa0b53be.svg' }
 ];
 
 const imageDarkOptions: DropdownOption[] = [
@@ -155,6 +156,7 @@ const imageDarkOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Heroku', value: 'https://brand.heroku.com/static/media/heroku-logo-stroke-gradient.bb410472.svg' }
 ];
 
 
